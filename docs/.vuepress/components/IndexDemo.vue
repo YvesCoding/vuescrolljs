916625e7b363614c8cf82123194280f9c7487ead
wangyi7099 <724003548@qq.com>
2018-05-23 14:38:06 +0800

    fix: index demo

--- docs/.vuepress/components/IndexDemo.vue
+++ docs/.vuepress/components/IndexDemo.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="demo-container">
-      <vue-scroll :ops="ops">
-        <div class="demo-content">
-          <span class="demo-content-text">Hello World</span>
-        </div>
-      </vue-scroll>
+    <vue-scroll :ops="ops">
+      <div class="demo-content">
+        <span class="demo-content-text">Hello Vuescroll!</span>
+      </div>
+    </vue-scroll>
   </div>
 </template>
 <script>
-export default { 
+export default {
   data() {
     return {
       ops: {
@@ -16,26 +16,35 @@ export default {
           scrollingX: false
         }
       } // Your options
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="stylus">
-  .demo-container
-    width 100%
-    height 100px
-  .demo-content
-    position relative
-    width 100%
-    height 500px
-    background: linear-gradient(0deg,#fff,#d5ffc1);
-  .demo-content-text
-    display block
-    position absolute
-    width 100%
-    bottom 0
-    text-align center
-    color #2c3e50;
-    font-weight 300
+.demo-container {
+  width: 100%;
+  height: 100px;
+
+  .vuescroll-content {
+    width: 100% !important;
+  }
+}
+
+.demo-content {
+  position: relative;
+  width: 100%;
+  height: 500px;
+  background: linear-gradient(0deg, #fff, #d5ffc1);
+}
+
+.demo-content-text {
+  display: block;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  text-align: center;
+  color: #2c3e50;
+  font-weight: 300;
+}
 </style>
